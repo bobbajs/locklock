387ea97a8fc26bf2f9ba8374071c30aafd96871c
Diana Sutandie <dianasutandie@gmail.com>
2013-03-26 00:32:09 -0700

    Ready version to commit

--- StereoSystem.c
+++ StereoSystem.c
@@ -41,21 +41,19 @@ int main()
 	//dBTester();
 	update();
 
-
+	alt_up_char_buffer_clear(char_buffer);
 	struct Cursor* cursor = initCursor(10, 100);
 	//Test VGA Output
 	struct Image* testImg;
 	struct Image* testImg1;
 	while((testImg = loadSDImage("TEST.BMP")) == NULL);
 	//while((testImg1 = loadSDImage("ART3.BMP")) == NULL);
-	//draw(35, 35, testImg);
+	draw(35, 35, testImg);
 	//draw(0, 20, testImg1);
 	killImage(testImg);
-	//alt_up_char_buffer_string(char_buffer, "Initialization Completed", 27, 5);
+	alt_up_char_buffer_string(char_buffer, "Initialization Completed", 27, 5);
 	//graphicTester();
 	//Test End
-	struct Frame* mainFrame = initMainFrame();
-	mainFrame->drawFrame(mainFrame);
 
 	int i = 2;
 	int count = 0;
@@ -70,9 +68,6 @@ int main()
 		cmdProcessing(scheduler);
 		updateMixer();
 
-		if (count == 500000){
-			menuButtonCollide(mainFrame->elements[0]->buttons[1]);
-		}
 		//i = soundTester(i);
 
 		updateCursor(cursor, (int)x, 100);
